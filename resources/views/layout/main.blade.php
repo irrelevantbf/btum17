4617e6e2b822018af0008fe0a9f39064515c49a1
ilia.sekhniashvili.1@btu.edu.ge <Illiadaodisea2k>
2020-10-14 16:54:56 +0400

    commit

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>TechNews - HTML and CSS Template</title>
+
+    <!-- favicon -->
+    <link href="{{ assets('assets/img/favicon.png') }}" rel=icon>
+
+    <!-- web-fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,500' rel='stylesheet' type='text/css'>
+
+    <!-- Bootstrap -->
+    <link href="{{ assets('assets/css/bootstrap.min.css') }}" rel="stylesheet">
+
+    <!-- font-awesome -->
+    <link href="{{ assets('assets/fonts/font-awesome/font-awesome.min.css') }}" rel="stylesheet">
+    <!-- Mobile Menu Style -->
+    <link href="{{ assets('assets/css/mobile-menu.css') }}" rel="stylesheet">
+
+    <!-- Owl carousel -->
+    <link href="{{ assets('assets/css/owl.coursel.css') }}" rel="stylesheet">
+    <link href="{{ assets('assets/css/owl') }}" rel="stylesheet">
+    <!-- Theme Style -->
+    <link href="{{ assets('assets/css/bootstrap.min.css') }}" rel="stylesheet">
+
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar">
